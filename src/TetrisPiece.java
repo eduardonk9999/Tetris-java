bed38d7cd3796744a6186a9dbb546e696b51c903
eduardonk9999 <thiago.novak7@gmail.com>
2023-09-09 14:53:40 -0300

    refact: my tetris

--- src/TetrisPiece.java
+++
@@ -1,9 +0,0 @@
-public class TetrisPiece {
-    private TetrisBlock[] blocks;
-
-    public TetrisPiece(TetrisBlock[] blocks) {
-        this.blocks = blocks;
-    }
-
-    // Métodos para mover, girar, desenhar
-}
