3e590e1fe87b6dae22e5fc56d8f148c9a2b9484c
eduardonk9999 <thiago.novak7@gmail.com>
2023-09-09 15:01:16 -0300

    feat: nova classe Peca, e pacote base

---
+++ src/Peca.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+public class Peca {
+    public static Color[] Cores = { 
+        Color.GREEN, 
+        Color.ORANGE, 
+        Color.YELLOW, 
+        Color.CYAN, 
+        Color.BLUE, 
+        Color.MAGENTA, 
+        Color.WHITE 
+    };
+}
