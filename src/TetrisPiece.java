ea0fe50b5fedc10ebfa23481da689f615eee4173
eduardonk9999 <thiago.novak7@gmail.com>
2023-09-10 12:14:16 -0300

    feat: criando classes

---
+++ src/TetrisPiece.java
@@ -0,0 +1,4 @@
+public class TetrisPiece {
+    // Declare as informações da peça, como forma e cor
+    // Implemente métodos para girar a peça, obter sua forma, etc.
+}
---
+++ src/TetrosBlock.java
@@ -0,0 +1,14 @@
+// representando os blocos individuais das peças
+public class TetrosBlock {
+    private int x;
+    private int y;
+    private int color;
+
+    public TetrosBlock(int x, int y, int color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    // Métodos para obter e definir coordenadas e cor do bloco
+
+}
